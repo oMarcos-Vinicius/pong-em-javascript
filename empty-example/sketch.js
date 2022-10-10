4fb312215062683132559f4c07f69736421ba3d5
Marcos Vinícius Silva <110570833+oMarcos-Vinicius@users.noreply.github.com>
2022-10-10 19:07:19 -0300

    Add files via upload

---
+++ empty-example/sketch.js
@@ -0,0 +1,20 @@
+let xBolinha = 300;
+let yBolinha = 200;
+let diametreo = 15;
+
+let velocidadeXBolinha = 6;
+let velocidadeyBolinha = 6;
+
+function setup() {
+    var cnv = createCanvas(600, 400);
+    var x = (windowWidth - width) / 2;
+    var y = (windowHeight - height) / 2;
+    cnv.position(x, y);
+}
+
+function draw() {
+    background(0)
+    circle(xBolinha, yBolinha, diametreo)
+    xBolinha += velocidadeXBolinha;
+    yBolinha += velocidadeyBolinha;
+}
